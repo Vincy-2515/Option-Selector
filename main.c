594ac5b189bab61558c7fd0f58337a4308bfd461
Vincy <nothing>
2024-11-20 23:53:26 +0100

    modificato file main

--- main.c
+++ main.c
@@ -15,20 +15,17 @@ int main () {
     system("cls");
 
     option1 = initializeSelection(1);
+    if (option1 < 0) checkOption(option1);
 
     option2 = initializeSelection(2);
+    if (option2 < 0) checkOption(option1);
+
+    printf("\nPREMI [INVIO] PER CHIUDERE:");
+    getchar();
 
     printf("%s", MOVE_CUROSOR_TO_HOME);
     printf("%s", DEFAULT_COLORS);
     printf("%s", ERASE_SCREEN);
-
-    if (option1 < 0) checkOption(option1);
-    if (option2 < 0) checkOption(option1);
-    printf("\nop1: %d", option1);
-    printf("\nop2: %d", option2);
-
-    printf("\nPREMI [INVIO] PER CHIUDERE");
-    getchar();
 
     return 0;
 }
@@ -38,12 +35,16 @@ void checkOption (int option) {
     printf("%s", ERASE_SCREEN); */
     
     if (option == -1) {
-        printf("[ERRORE]: tasto premuto non valido");
+        printf("\n[ERRORE]: tasto premuto non valido");
     } 
     else if (option == -2) {
-        printf("[ERRORE]: coordinate non valide");
+        printf("\n[ERRORE]: coordinate non valide");
     }
     else {
-        printf("[ERRORE]: non definito");
+        printf("\n[ERRORE]: non definito");
     }
+
+    printf("\noption: %d", option);
+    printf("\nPREMI [INVIO] PER PROSEGUIRE:");
+    getchar();
 }
