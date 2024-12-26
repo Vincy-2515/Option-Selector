eafb0dd1656f5e9799706d0b68728f0698651139
Vincy-2515 <110780936+Vincy-2515@users.noreply.github.com>
2024-12-27 00:40:32 +0100

    modificato file header

--- src/selection.h
+++ src/selection.h
@@ -1,12 +1,13 @@
 #ifndef SELECTION_H    
 #define SELECTION_H    
-    int initializeSelection(int requested_option_list);
-    void getSettings(int list_choice);
-    int verifySelectedOptionCoords(int x, int y);
-    void checkGridLimitOverflow (int *x, int *y);
-    char **getStrings(char **options_strings);
-    void printOptionsStrings(char **options_strings, int selected_option);
+    int initializeSelection (int requested_option_list);
+    void getSettings (int list_choice);
+    int getStrings (char **options_strings);
+    int printOptionsStrings (char **options_strings, int selected_option);
     void printOnOnlyColumnsGrid (char **options_strings, int selected_option);
     void printOnOnlyRowsGrid (char **options_strings, int selected_option);
     void printOnGrid (char **options_strings, int selected_option);
+    void checkGridLimitOverflow (int *x, int *y);
+    int verifySelectedOptionCoords (int *options_coords, int x, int y);
+    void coordGenerator (int *options_coords);
 #endif
