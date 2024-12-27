1539e1fa2e3d4eb0514c00d43ecc916f502322bd
Vincy-2515 <110780936+Vincy-2515@users.noreply.github.com>
2024-12-27 18:23:27 +0100

    migliorata leggibilità, modificata gestione delle impostazioni, migliorata gestione degli errori

--- src/selection.h
+++ src/selection.h
@@ -1,25 +1,24 @@
 #ifndef SELECTION_H
-#define SELECTION_H
+    #define SELECTION_H
     typedef struct {
         int use_columns;
         int use_rows;
         int max_options;
         int max_columns;
         int max_rows;
-        int max_line_length;
-
-        //percorso dei file di impostazioni
-        char strings_filename[200];
+        int max_option_string_length;
+        char *path;
     } Settings;
 
-    int initializeSelection (Settings settings);
-    //void checkSettings (Settings settings);
+    int initializeSelection (int use_columns, int use_rows, int max_options, int max_columns, int max_rows, int max_option_string_length, char *path);
+    void setSettings (Settings *p_settings,int use_columns, int use_rows, int max_options, int max_columns, int max_rows, int max_option_string_length, char *path);
+    int checkSettings (Settings settings);
     int printOptionsStrings (Settings settings, char **options_strings, int selected_option);
     int getStrings (Settings settings, char **options_strings);
     void printOnOnlyColumnsGrid (Settings settings, char **options_strings, int selected_option);
     void printOnOnlyRowsGrid (Settings settings, char **options_strings, int selected_option);
     void printOnGrid (Settings settings, char **options_strings, int selected_option);
-    void checkGridLimitOverflow (Settings settings, int *x, int *y);
+    void checkGridLimitOverflow (Settings settings, int *p_x, int *p_y);
     int verifySelectedOptionCoords (Settings settings, int *options_coords, int x, int y);
     void coordGenerator (Settings settings, int *options_coords);
 #endif
