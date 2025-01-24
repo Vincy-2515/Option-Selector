a82b8fead9c8f33a0b9f3640da2dd12d3cf4f3fd
vincenzoscarso <110780936+vincenzoscarso@users.noreply.github.com>
2025-01-24 10:03:42 +0100

    .

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main( void ){
     char path_1[] = "src/resources/options_strings/selection_one.txt";
     char path_2[] = "src/resources/options_strings/selection_two.txt";
 
-    option1 = initializeSelection(1, 0, 8, 8, 1, 13, path_1, 0, 0);
+    option1 = initializeSelection(1, 1, 8, 3, 3, 13, path_1, 0, 0);
     option2 = initializeSelection(0, 1, 2, 1, 2, 13, path_2, 0, 0);
 
     printf("OPZIONI SELEZIONATE (a partire da 0):");
